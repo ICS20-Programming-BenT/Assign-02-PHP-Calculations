ff688a5acf20fe5cade8c2c5c1282ba5100817c5
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-03-26 18:11:10 +0000

    Fixed more errors

--- calculations.php
+++ calculations.php
@@ -7,7 +7,8 @@
   //Perform mathematical equations
   $volumeOfCone = (pi()) * $radiusOfCone ** 2 * $heightOfCone / 3;
   $surfaceAreaOfCone = (pi()) * $radiusOfCone * ($radiusOfCone + (sqrt($heightOfCone ** 2 + $radiusOfCone ** 2))
+
 ?>
 
-The volume of the cone is <?php echo round($volumeOfCone, 2) ?><br>
-The surface area of the cone is <?php echo round($surfaceAreaOfCone, 2) ?>
+  The volume of the cone is <?php echo round($volumeOfCone, 2) ?><br>
+  The surface area of the cone is <?php echo round($surfaceAreaOfCone, 2) ?>
