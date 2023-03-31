23a5033e666ed43126c8aad947b233d648f4df36
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-03-31 14:52:00 +0000

    Attempted one last time to fix favicon

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!-- Code for the favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="./fav_index1/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./fav_index1/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./fav_index1/favicon-16x16.png">
-    <link rel="manifest" href="./fav_index1/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="manifest" href="./favicon/site.webmanifest">
 
     <!-- Google's MDL -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
